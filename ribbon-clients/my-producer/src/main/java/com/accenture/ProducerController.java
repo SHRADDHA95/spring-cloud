f91cb84e3b9afe686e71eeb575657464f47edafd
Shraddha Shrivastav <shraddha.shrivastav@accenture.com>
2023-05-02 12:39:07 +0530

    ribbon-client demo

---
+++ ribbon-clients/my-producer/src/main/java/com/accenture/ProducerController.java
@@ -0,0 +1,19 @@
+package com.accenture;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller			//monolithic (spring mvc)
+@RestController		//microservices (spring rest/spring boot)
+public class ProducerController {
+	
+	@Value("${server.port}")
+	String port;
+	
+	@RequestMapping("/welcomeproducer")
+	public String producerWelcomeMethod() {
+		return "this message is from producer running on port : " + port ;
+	}
+}
